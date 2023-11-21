407808ad43e65b7c44e48280a0c374084656bbd2
ccollinsibe <ccollinsibe@yahoo.com>
2023-11-21 08:04:30 +0100

    reset for service containers

--- data/database.js
+++ data/database.js
@@ -7,8 +7,9 @@ const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
 const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
-const uri2 = `mongodb://abc:${dbPassword}@ac-hvwu9tf-shard-00-00.ldmnxb4.mongodb.net:27017,ac-hvwu9tf-shard-00-01.ldmnxb4.mongodb.net:27017,ac-hvwu9tf-shard-00-02.ldmnxb4.mongodb.net:27017/?ssl=true&replicaSet=atlas-rbju6t-shard-0&authSource=admin&retryWrites=true&w=majority`
-const client = new MongoClient(uri2);
+const client = new MongoClient(uri);
+//const uri2 = `mongodb://abc:${dbPassword}@ac-hvwu9tf-shard-00-00.ldmnxb4.mongodb.net:27017,ac-hvwu9tf-shard-00-01.ldmnxb4.mongodb.net:27017,ac-hvwu9tf-shard-00-02.ldmnxb4.mongodb.net:27017/?ssl=true&replicaSet=atlas-rbju6t-shard-0&authSource=admin&retryWrites=true&w=majority`
+//const client = new MongoClient(uri2);
 
 console.log('Trying to connect to db');
 
